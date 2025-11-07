4ec9e70b96635e321b2066b3e1a2bb508775a414
Dipesh <pundipesh223@gmail.com>
2025-11-07 15:17:29 +0545

    feat:adding homepage

---
+++ src/components/HomeMainPage/Homepage.jsx
@@ -0,0 +1,19 @@
+import Navbar from '../Navbar/Navbar'
+import Body from './Body/Body'
+import Cards from './Cards/Cards'
+import Accessories  from './Accessories/Accessories'
+import Footer from '../Footer/Footer'
+
+function Homepage(){
+  return (
+    <>
+    <Navbar/>
+    <Body/>
+    <Cards/>
+    <Accessories/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Homepage;
